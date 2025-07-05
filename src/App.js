221e0043d842d75530ce98c2e24d4eda2d89760a
Jason Yang <97807901+h3yJ@users.noreply.github.com>
2025-07-04 22:50:54 -0400

    Initialize react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Arena God Tracker</h1>
+      <p>Track which champs you’ve gotten 1st place with in League Arena mode!</p>
+    </div>
+  );
+}
+
+export default App;
